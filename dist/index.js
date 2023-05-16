d4e2fbd852b01288d334d7507b1d9cd99c88bea3
mahmoudgomaa99 <mahmoud.gomaa14@gmail.com>
2023-05-16 15:50:52 +0300

    port edited

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ var checkToken_1 = __importDefault(require("./middlewares/checkToken"));
 var Todos_1 = __importDefault(require("./routes/Todos"));
 require("dotenv").config();
 var app = (0, express_1.default)();
-var PORT = 3000;
+var PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/api/users", Users_1.default);
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import TodosRouter from "./routes/Todos";
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
